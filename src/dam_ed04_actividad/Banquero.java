0d548cddffb74bb5a21a4a1c6e78a484e1e2f0b7
DPTOSISTEMAS <DPTOSISTEMAS@192.168.1.105>
2016-06-10 18:31:39 +0100

    Añadimos getter y setter a la clase banquero

--- src/dam_ed04_actividad/Banquero.java
+++ src/dam_ed04_actividad/Banquero.java
@@ -22,5 +22,30 @@ public class Banquero {
         this.nombre = nombre;
         this.DNI = DNI;
     }
+   public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
     
+    
+     
 }
