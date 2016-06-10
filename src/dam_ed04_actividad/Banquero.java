7b85eb71c2737ae6369113c1475112d5d239c3ff
DPTOSISTEMAS <DPTOSISTEMAS@192.168.1.105>
2016-06-10 18:30:52 +0100

    Creacion constructores clase banquero

---
+++ src/dam_ed04_actividad/Banquero.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dam_ed04_actividad;
+
+/**
+ *
+ * @author DPTOSISTEMAS
+ */
+public class Banquero {
+    
+    private String nombre;
+    private String apellidos;
+    private String DNI;
+
+    public Banquero() {
+    }
+
+    public Banquero(String nombre, String DNI) {
+        this.nombre = nombre;
+        this.DNI = DNI;
+    }
+    
+}
